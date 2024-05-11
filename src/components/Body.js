3b04cb0b849201c5eae3623991c748d29cd35eb8
Harsh Kumar <vijayk8478@gmail.com>
2024-05-11 15:55:30 +0530

    tailwind configured

---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './Login'
+import Browse from './Browse'
+
+const Body = () => {
+  return (
+    <>
+    <Login />
+    <Browse />
+  </>
+  )
+}
+
+export default Body
