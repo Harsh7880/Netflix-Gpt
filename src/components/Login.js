0fd5a10ce8da6c55f28083995cd2b37915d967ab
Harsh Kumar <vijayk8478@gmail.com>
2024-09-02 17:22:52 +0530

    form validation Added for username, email and password

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,24 @@
 import React from "react";
 import Header from "./Header";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { checkValidateData } from "../utils/validate";
 
 const Login = () => {
 
   const [isSignInFrom,setIsSignInFrom] = useState(true);
+  const [errorMesaage, setErrorMessage] = useState(null);
+  
+  const fullName = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+
+    const message = checkValidateData(email.current.value,password.current.value,fullName.current.value);
+    setErrorMessage(message);
+  }
+
+  
 
   const toggleSignInfrom = () => {
     setIsSignInFrom(!isSignInFrom);
@@ -19,12 +33,13 @@ const Login = () => {
           alt="backgound"
         />
       </div>
-      <form className="text-white w-3/12 absolute p-12 bg-black	 my-44 mx-auto to right-0 left-0 bg-opacity-85">
+      <form onSubmit={(e) => {e.preventDefault()}} className="text-white w-3/12 absolute p-12 bg-black	 my-44 mx-auto to right-0 left-0 bg-opacity-85">
         <h1 className="text-3xl py-4"> { isSignInFrom ? "Sign In" : "Sign Up "}</h1>
        
         {
         !isSignInFrom &&(
           <input
+          ref={fullName}
           type="text"
           placeholder="Full Name"
           className="rounded w-full p-3 my-2 bg-gray-700"
@@ -33,18 +48,25 @@ const Login = () => {
        }
 
          <input
+          ref={email}
           type="text"
           placeholder="Email or phone number"
           className="rounded w-full p-3 my-2 bg-gray-700"
         />
        
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="rounded w-full p-3 my-2 bg-gray-700"
         />
-        <button className="w-full p-2 my-5  bg-red-700 rounded">
-          {isSignInFrom ? "Sign In" : "Sign Up "}
+
+        <p className="text-red-500 font-bold test-lg py-2">{errorMesaage}</p>
+        <button 
+        onClick={handleButtonClick}
+        className="w-full p-2 my-5  bg-red-700 rounded">
+        {isSignInFrom ? "Sign In" : "Sign Up "}
+          
         </button>
         <p className="py-4 cursor-pointer" 
         onClick={toggleSignInfrom}
---
+++ src/utils/validate.js
@@ -0,0 +1,12 @@
+export const checkValidateData = (email,password,fullName) => {
+
+    const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+    const isNamevalid = /(^[A-Za-z]{3,16})([ ]{0,1})([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})/.test(fullName);
+
+    if(!isNamevalid) return "Full Name in not valid"
+    if(!isEmailValid) return "Email ID is not valid";
+    if(!isPasswordValid) return "Password is not valid"
+
+    return null;
+}
